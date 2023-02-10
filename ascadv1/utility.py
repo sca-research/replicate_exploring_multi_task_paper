65a568fad011d49c05f7efb1e0521f715559fadd
TMarquet <72377479+TMarquet@users.noreply.github.com>
2023-02-10 22:31:36 +0100

    Update utility.py

--- ascadv1/utility.py
+++ ascadv1/utility.py
@@ -7,28 +7,23 @@ Created on Tue Feb  1 14:49:32 2022
 
 
 import os
-from scipy import stats
 import pickle
 import h5py
 
 
 import numpy as np
-import re
-import scipy.signal as sig
-import argparse
+
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' 
 
 import tensorflow as tf
-import tensorflow.keras.backend as K
 import tensorflow.experimental.numpy as tnp
 
 from tensorflow.keras.layers import LayerNormalization,BatchNormalization
 
 tnp.experimental_enable_numpy_behavior()
 
-from utils.generate_intermediate_values import save_real_values, multGF256
-import tensorflow_probability as tfp
+
 from gmpy2 import f_divmod_2exp
 
 # Opening dataset specific variables 
@@ -133,77 +128,7 @@ class XorLayer(tf.keras.layers.Layer):
         base_config = super(XorLayer,self).get_config()
         return dict(list(base_config.items()) + list(config.items()))
     
-log_table=[ 0, 0, 25, 1, 50, 2, 26, 198, 75, 199, 27, 104, 51, 238, 223, 3,
-    100, 4, 224, 14, 52, 141, 129, 239, 76, 113, 8, 200, 248, 105, 28, 193,
-    125, 194, 29, 181, 249, 185, 39, 106, 77, 228, 166, 114, 154, 201, 9, 120,
-    101, 47, 138, 5, 33, 15, 225, 36, 18, 240, 130, 69, 53, 147, 218, 142,
-    150, 143, 219, 189, 54, 208, 206, 148, 19, 92, 210, 241, 64, 70, 131, 56,
-    102, 221, 253, 48, 191, 6, 139, 98, 179, 37, 226, 152, 34, 136, 145, 16,
-    126, 110, 72, 195, 163, 182, 30, 66, 58, 107, 40, 84, 250, 133, 61, 186,
-    43, 121, 10, 21, 155, 159, 94, 202, 78, 212, 172, 229, 243, 115, 167, 87,
-    175, 88, 168, 80, 244, 234, 214, 116, 79, 174, 233, 213, 231, 230, 173, 232,
-    44, 215, 117, 122, 235, 22, 11, 245, 89, 203, 95, 176, 156, 169, 81, 160,
-    127, 12, 246, 111, 23, 196, 73, 236, 216, 67, 31, 45, 164, 118, 123, 183,
-    204, 187, 62, 90, 251, 96, 177, 134, 59, 82, 161, 108, 170, 85, 41, 157,
-    151, 178, 135, 144, 97, 190, 220, 252, 188, 149, 207, 205, 55, 63, 91, 209,
-    83, 57, 132, 60, 65, 162, 109, 71, 20, 42, 158, 93, 86, 242, 211, 171,
-    68, 17, 146, 217, 35, 32, 46, 137, 180, 124, 184, 38, 119, 153, 227, 165,
-    103, 74, 237, 222, 197, 49, 254, 24, 13, 99, 140, 128, 192, 247, 112, 7 ]
 
-alog_table =[1, 3, 5, 15, 17, 51, 85, 255, 26, 46, 114, 150, 161, 248, 19, 53,
-    95, 225, 56, 72, 216, 115, 149, 164, 247, 2, 6, 10, 30, 34, 102, 170,
-    229, 52, 92, 228, 55, 89, 235, 38, 106, 190, 217, 112, 144, 171, 230, 49,
-    83, 245, 4, 12, 20, 60, 68, 204, 79, 209, 104, 184, 211, 110, 178, 205,
-    76, 212, 103, 169, 224, 59, 77, 215, 98, 166, 241, 8, 24, 40, 120, 136,
-    131, 158, 185, 208, 107, 189, 220, 127, 129, 152, 179, 206, 73, 219, 118, 154,
-    181, 196, 87, 249, 16, 48, 80, 240, 11, 29, 39, 105, 187, 214, 97, 163,
-    254, 25, 43, 125, 135, 146, 173, 236, 47, 113, 147, 174, 233, 32, 96, 160,
-    251, 22, 58, 78, 210, 109, 183, 194, 93, 231, 50, 86, 250, 21, 63, 65,
-    195, 94, 226, 61, 71, 201, 64, 192, 91, 237, 44, 116, 156, 191, 218, 117,
-    159, 186, 213, 100, 172, 239, 42, 126, 130, 157, 188, 223, 122, 142, 137, 128,
-    155, 182, 193, 88, 232, 35, 101, 175, 234, 37, 111, 177, 200, 67, 197, 84,
-    252, 31, 33, 99, 165, 244, 7, 9, 27, 45, 119, 153, 176, 203, 70, 202,
-    69, 207, 74, 222, 121, 139, 134, 145, 168, 227, 62, 66, 198, 81, 243, 14,
-    18, 54, 90, 238, 41, 123, 141, 140, 143, 138, 133, 148, 167, 242, 13, 23,
-    57, 75, 221, 124, 132, 151, 162, 253, 28, 36, 108, 180, 199, 82, 246, 1 ]
-
-# Multiplication function in GF(2^8)
-def multGF256(a,b):
-    if (a==0) or (b==0):
-        return 0
-    else:
-        return alog_table[(log_table[a]+log_table[b]) %255]
-    
-class MultiLayer(tf.keras.layers.Layer):
-    def __init__(self,classes = 256 ,name = ''):
-        super(MultiLayer, self).__init__(name = name)
-        all_maps = np.load('utils/mult_mapping.npy')
-        mapping1 = []
-        mapping2 = []
-        for classe in range(classes):
-            mapped = np.where(all_maps[classe] == 1)
-            mapping1.append(mapped[0])
-            mapping2.append(mapped[1])
-        self.mapping1 = np.array(mapping1)
-        self.mapping2 = np.array(mapping2)
-        self.classes = classes
-    
-    def call(self, inputs):  
- 
-        pred1 = tnp.asarray(inputs[0])
-        pred2 = tnp.asarray(inputs[1])
-        p1 = pred1[:,self.mapping1]
-        p2 = pred2[:,self.mapping2]
-    
-        res = tf.reduce_sum(tf.multiply(p1,p2),axis =2)   
-        return res
-
-    def get_config(self):
-        config = {'mapping':self.mapping,
-                  'classes':self.classes}
-        base_config = super(MultiLayer,self).get_config()
-        base_config.update(config)
-        return base_config
 
 
 
@@ -357,19 +282,20 @@ def load_dataset(target,intermediate,n_traces = None,load_masks = False,window_t
     training = dataset == 'training' 
     if print_logs :
         str_targets = 'Loading samples and labels for {}'.format(target)
+        print(str_targets)
+        
         
     traces , labels_dict = read_from_h5_file(n_traces=n_traces,dataset = dataset)
     traces_val = np.expand_dims(traces,2)
     X_profiling_dict = {}  
-    X_profiling_dict['traces'] = traces if intermediate in ['p','rin','alpha','beta'] else traces[:,3150:3350]
+    X_profiling_dict['traces'] = traces 
 
-    
+
     if training:
         traces_val , labels_dict_val = read_from_h5_file(n_traces=n_traces,dataset = 'test')
         traces_val = np.expand_dims(traces_val,2)
         X_validation_dict = {}  
-        X_validation_dict['traces'] = traces_val if intermediate in ['p','rin','alpha','beta'] else traces_val[:,3150:3350]
-        
+        X_validation_dict['traces'] = traces_val
 
     Y_profiling_dict = {}
     real_values = np.array(labels_dict[intermediate],dtype = np.uint8)[:n_traces]
@@ -480,9 +406,6 @@ def load_dataset_hierarchical(byte,n_traces = 250000,multi_target =False,dataset
     X_profiling_dict = {}  
     X_profiling_dict['traces'] = traces 
 
-    
-  
-    permutations = np.array(labels_dict['p'],dtype = np.uint8)[:n_traces]
 
     if training:
         traces_val , labels_dict_val = read_from_h5_file(n_traces=n_traces,dataset = 'test')
@@ -490,9 +413,6 @@ def load_dataset_hierarchical(byte,n_traces = 250000,multi_target =False,dataset
         X_validation_dict = {}  
         X_validation_dict['traces'] = traces_val
         
-        permutations_val = np.array(labels_dict_val['p'],dtype = np.uint8)
-        
-
 
     if print_logs :
         print('Loaded inputs')    
