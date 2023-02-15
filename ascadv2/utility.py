2f1ef2d4543f47708515c468570efc50c560f076
TMarquet <72377479+TMarquet@users.noreply.github.com>
2023-02-15 21:17:35 +0100

    Update utility.py

--- ascadv2/utility.py
+++ ascadv2/utility.py
@@ -249,10 +249,16 @@ class Add_Shares(tf.keras.layers.Layer):
         self.input_dim = input_dim
         self.shares = shares
 
+        self.selu = tf.keras.activations.selu
+        self.layer_normalization = LayerNormalization()        
+        
+
     def call(self, inputs):  
+        activated_inputs = self.selu(inputs)
+        normalised_inputs = tf.add(self.layer_normalization(activated_inputs),2)
         out = self.b        
         for share in range(self.shares):
-            out = out + tf.matmul(inputs[share],self.w[share])
+            out = out + tf.matmul(normalised_inputs[share],self.w[share])
         return out
 
     def get_config(self):
